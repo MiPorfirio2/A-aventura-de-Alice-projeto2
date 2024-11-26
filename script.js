e9f1c56cd17c09d52722d9b5529ddab4fe074fd1
MiPorfirio2 <00001101238811sp@al.educacao.sp.gov.br>
2024-11-26 04:49:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const avança = document.querySelectorAll('.btn-proximo');
+
+avança.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+atual.classList.remove('tivo');
+document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
